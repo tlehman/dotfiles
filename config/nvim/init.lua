94b58670b4dd49b1ab14312e38c8051ea27b4509
Tobi Lehman <mail@tobilehman.com>
2025-04-15 15:33:46 -0700

    nvim: add key mappings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,3 @@
-require("config.lazy")
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.number = true
@@ -6,5 +5,16 @@ vim.opt.number = true
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
 
+-- key mappings
+vim.g.mapleader = ","
+vim.api.nvim_set_keymap('n',   -- n for normal mode
+        '<Leader>w', ':w<CR>',
+        { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap('n',   -- n for normal mode
+        '<Leader>q', ':q<CR>',
+        { noremap = true, silent = true }
+)
+
+require("config.lazy")
