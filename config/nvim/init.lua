be2c5668f44072555947dad5e3671718fa9a4930
Tobi Lehman <mail@tobilehman.com>
2025-04-15 18:57:30 -0700

    nvim tab width 2 and dark mode

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,16 +5,13 @@ vim.opt.number = true
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
 -- key mappings
 vim.g.mapleader = ","
-vim.api.nvim_set_keymap('n',   -- n for normal mode
-        '<Leader>w', ':w<CR>',
-        { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap('n',   -- n for normal mode
-        '<Leader>q', ':q<CR>',
-        { noremap = true, silent = true }
-)
+opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap('n', '<Leader>w', ':w<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>q', ':q<CR>', opts)
 
 require("config.lazy")
