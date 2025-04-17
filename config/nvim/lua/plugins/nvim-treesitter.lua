4e00ceb4e034eada3ddcca0bed53a1310bb17c18
Tobi Lehman <mail@tobilehman.com>
2025-04-16 21:55:09 -0700

    Add haskell syntax to nvim

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "go", "nix" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "haskell", "javascript", "html", "go", "nix" },
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },  
