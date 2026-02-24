6adee1591359af87470545321253408fe540548e
Tobi Lehman <261552+tlehman@users.noreply.github.com>
2026-02-23 16:22:40 -0800

    Add Podman Desktop and podman command

---
+++ modules/home-manager/podman-desktop/default.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.tlehman.podman-desktop.enable = lib.mkEnableOption "Podman Desktop";
+  config = lib.mkIf config.tlehman.podman-desktop.enable {
+    home.packages = builtins.attrValues {
+      inherit (pkgs) podman podman-desktop;
+    };
+
+    xdg.configFile."containers/containers.conf".source = config.lib.file.mkOutOfStoreSymlink (
+      config.home.homeDirectory + "/src/tlehman/dotfiles/config/containers/containers.conf"
+    );
+
+    xdg.dataFile."containers/podman-desktop/configuration/settings.json".source =
+      config.lib.file.mkOutOfStoreSymlink
+        (
+          config.home.homeDirectory + "/src/tlehman/dotfiles/config/containers/podman-desktop-settings.json"
+        );
+  };
+}
