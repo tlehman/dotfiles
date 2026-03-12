f811d9ba3bf27f8618e5e90ca9777280f00d3093
Tobi Lehman <261552+tlehman@users.noreply.github.com>
2026-03-12 16:26:20 -0700

    Add cmake, pandoc, mermaid-cli, texlive, ts lsp

--- modules/darwin/mixins/homebrew.nix
+++ modules/darwin/mixins/homebrew.nix
@@ -15,7 +15,7 @@
         "obsidian"
         "tailscale"
       ];
-      brews = [ "fastfetch" "rainfrog" "temporal" ];
+      brews = [ "cmake" "fastfetch" "pandoc" "mermaid-cli" "texlive" "rainfrog" "temporal" "typescript-language-server"];
     };
   };
 }
